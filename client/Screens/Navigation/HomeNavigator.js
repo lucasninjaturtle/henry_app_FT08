9ed5c0614c08feeafe33f727d4eff4d70afb112c
unknown <lucas.a.fernandez.morales@gmail.com>
2021-02-25 20:16:32 -0300

    Fix/ called home component on homeNavigator Stack

--- client/Screens/Navigation/HomeNavigator.js
+++ client/Screens/Navigation/HomeNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
 import Dummy from './Dummy'
+import Home from '../Home/Home'
 
 const Stack = createStackNavigator()
 
@@ -11,7 +12,7 @@ const MyStack = ()=>{
         <Stack.Navigator>
             <Stack.Screen
             name='Home'
-            component={Dummy}
+            component={Home}
             options={{
                 headerShown: false,
             }}
