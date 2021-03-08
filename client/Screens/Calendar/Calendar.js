9af6dc6d421b46dc117ae8fdca4b84ae8f92e7b5
unknown <lucas.a.fernandez.morales@gmail.com>
2021-03-08 12:33:09 -0300

    changed cakendar to agenda

--- client/Screens/Calendar/Calendar.js
+++ client/Screens/Calendar/Calendar.js
@@ -5,28 +5,19 @@ import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 const Calendario = ()=>{
     const [selectedDate, setSelectedDate] = useState('')
     return (
-        <Container style={{alignContent:'center', alignItems:'center', paddingTop:60, backgroundColor:'#FFFDD0'}}>
-            <Content>
-                <CardItem bordered>
-                    <View>
-                        <Text>Este es el calendario</Text>
-                    </View>
-                </CardItem>
-                <CardItem bordered>
-                    <Calendar 
-                        onDayPress={(day) => {setSelectedDate(day.dateString)}}
-                        theme={{
-                            selectedDayBackgroundColor: '#00adf5',
-                        }}
-                        markedDates={{
-                            [selectedDate]: {selected: true, selectedColor: '#00adf5'},
-                        }}
-                    />
-                </CardItem>
-                <CardItem>
-                    <Text>{selectedDate}</Text>
-                </CardItem>
-            </Content>
+        <Container style={{alignContent:'center', paddingTop:60, backgroundColor:'#FFFDD0'}}>
+            <Agenda
+            items={items ? items : {}}
+
+            loadItemsForMonth={loadItems}
+
+            renderEmptyDate={renderEmptyDate}            
+            //renderDay={renderDays}
+            renderItem={renderItem}
+            //rowHasChanged={(r1, r2)=> r1.name !== r2.name}
+            pastScrollRange={12}
+            futureScrollRange={12}
+            />
         </Container>
     )
 }
