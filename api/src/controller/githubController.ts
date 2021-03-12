17d0303d01a8b91221e9654343222b9a6c183bac
MarcosGrizzuti <marcos.grizzuti@gmail.com>
2021-03-12 00:29:33 -0300

    GET a repositorios y commits funcional

--- api/src/controller/githubController.ts
+++ api/src/controller/githubController.ts
@@ -11,40 +11,42 @@ export const githubController = {
       })
         .then((getUserAllGHRepos) => {
           var privateRepos = []
-          getUserAllGHRepos.data.map((UserGHRep) => {
+          var commitsArray = []
+          var arrayPromises = []
+
+          getUserAllGHRepos.data.forEach((UserGHRep) => {
             if(UserGHRep.fork == true) {
-              if(UserGHRep.name == "Curso.Prep.Henry" ||UserGHRep.name == "FT-M1" || UserGHRep.name == "FT-M2" || UserGHRep.name == "FT-M3" || UserGHRep.name == "FT-M4" || UserGHRep.name.slice(0, 12) == "ecommerce-ft") {
-                /* privateRepos.push(UserGHRep.name) */
-                axios.get(`https://api.github.com/repos/${UserGHRep.owner.login}/${UserGHRep.name}/commits`, {
-                headers: {
-                  "Authorization": `Bearer 9bf31ce262244fece1b4b5633c841f56c367abf8`
-                  }
-                })
-                  .then((getCommits) => {
-                    let commitsArray = []
-                    getCommits.data.map((commit) => {
-                      commitsArray.push({
-                        author: commit.commit.author.name,
-                        date: commit.commit.author.date,
-                        message: commit.commit.message,
-                        commitHash: commit.sha                      })
-                    })
-                    let infoFiltrada = {
-                      name: UserGHRep.name,
-                      data: commitsArray
+              if(UserGHRep.name == "Curso.Prep.Henry" ||UserGHRep.name == "FT-M1" || UserGHRep.name == "FT-M2" || UserGHRep.name == "FT-M3" || UserGHRep.name == "FT-M4" ||
+              UserGHRep.name.slice(0, 12) == "ecommerce-ft") {
+                arrayPromises.push(axios.get(`https://api.github.com/repos/${UserGHRep.owner.login}/${UserGHRep.name}/commits`, {
+                  headers: {
+                    "Authorization": `Bearer 9bf31ce262244fece1b4b5633c841f56c367abf8`
                     }
-                    console.log(infoFiltrada)
-                  })
-                  .catch((err) => {
-                    console.log("El error es: " + err)
-                  })
+                }))
+
+                privateRepos.push({
+                  name: UserGHRep.name,
+                  data: null
+                })
+                }
               }
-            }
-          })
+            })
+
+            Promise.all(arrayPromises).then(r => { // r = array de promesas (repos)
+              r.forEach((obj, i) => { // por cada repo (obj = array de commits)
+                commitsArray = []
+                obj.data.forEach(commit => {
+                  commitsArray.push({
+                  author: commit.commit.author.name,
+                  date: commit.commit.author.date,
+                  message: commit.commit.message,
+                  commitHash: commit.sha
+                  })
+                })
+                privateRepos[i].data = commitsArray
+              })
+              res.json(privateRepos)
+            })
         })
   },
 };
-
-/* commit.author.name
-  commit.author.date
-  commit.message */
