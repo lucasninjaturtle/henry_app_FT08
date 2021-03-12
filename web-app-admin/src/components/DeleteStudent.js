ef65784aed411ef0ac8ffbfd9b96cbb9c6822673
ldiaz89 <lgdt66@gmail.com>
2021-03-12 08:15:14 -0400

    delete student

---
+++ web-app-admin/src/components/DeleteStudent.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react'
+import axios from "axios";
+import AsyncSelect from "react-select/async";
+
+
+function DeleteStudent() {
+    return (
+        <div>
+            Delete
+        </div>
+    )
+}
+
+export default DeleteStudent
