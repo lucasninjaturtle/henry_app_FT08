a63cc1714120165531f1448365893df76b80e67b
lucasmercado101 <lucasmercado101@gmail.com>
2021-03-13 18:25:00 -0300

    fix error

--- client/Screens/Home/Home.js
+++ client/Screens/Home/Home.js
@@ -1,26 +1,45 @@
-import React, { useEffect } from 'react'
-import 'react-native-gesture-handler';
-import { Image, StyleSheet } from 'react-native'
-import { Container, Icon, Content, Card, CardItem, Text, Switch, Body, Button, View, Thumbnail, List, ListItem, Left, Right } from "native-base";
-import { useSelector, useDispatch } from 'react-redux'
-import { getUserInfo } from '../../Redux/Actions/userActions';
-import store from '../../Redux/store';
-import { ScrollView } from 'react-native-gesture-handler';
-import Profile from '../Profile/Profile'
+import React, { useEffect } from "react";
+import "react-native-gesture-handler";
+import { Image, StyleSheet } from "react-native";
+import {
+  Container,
+  Icon,
+  Content,
+  Card,
+  CardItem,
+  Text,
+  Switch,
+  Body,
+  Button,
+  View,
+  Thumbnail,
+  List,
+  ListItem,
+  Left,
+  Right
+} from "native-base";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserInfo } from "../../Redux/Actions/userActions";
+import store from "../../Redux/store";
+import { ScrollView } from "react-native-gesture-handler";
+import Profile from "../Profile/Profile";
 
-import StudentCard from "./StudentCard"
-
-
+import StudentCard from "./StudentCard";
 
 export default function Home({ navigation }) {
-
-  let student = useSelector((store) => store.userInfo.usuario)
+  let student = useSelector((store) => store.userInfo.usuario);
 
   return (
     <ScrollView>
       <Container style={styles.container}>
         <CardItem header bordered style={styles.card}>
-          <Thumbnail style={styles.image} source={{ uri: 'https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png' }} />
+          <Thumbnail
+            style={styles.image}
+            source={{
+              uri:
+                "https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png"
+            }}
+          />
         </CardItem>
         <StudentCard data={student} />
       </Container>
@@ -83,26 +102,22 @@ export default function Home({ navigation }) {
         </List>
       </Content>
     </ScrollView>
-
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignContent: 'center',
-    alignItems: 'center',
+    alignContent: "center",
+    alignItems: "center",
     paddingTop: 10,
-    backgroundColor: '#AED6F1',
+    backgroundColor: "#AED6F1",
     height: 550
-
-
   },
   card: {
-    alignItems: 'center',
-    alignContent: 'center',
-    alignSelf: 'center',
-    backgroundColor: '#AED6F1'
-
+    alignItems: "center",
+    alignContent: "center",
+    alignSelf: "center",
+    backgroundColor: "#AED6F1"
   },
   image: {
     borderRadius: 50,
@@ -111,11 +126,11 @@ const styles = StyleSheet.create({
     marginTop: 10
   },
   titles: {
-    fontFamily: 'monospace',
-    fontStyle: 'normal',
-    fontSize: 20,
+    fontFamily: "monospace",
+    fontStyle: "normal",
+    fontSize: 20
   },
   list: {
     paddingTop: -100
   }
-
+});
