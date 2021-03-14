3ea168bc1ddbccb113f85be9566662348382e5a9
lautaro202 <lautaropaez308@gmail.com>
2021-03-14 19:10:53 -0300

    datos reales

--- client/Screens/Home/StudentCard.jsx
+++ client/Screens/Home/StudentCard.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { Content, CardItem, Text, Thumbnail, Container } from "native-base";
+import {
+  Content,
+  CardItem,
+  Text,
+  Thumbnail,
+  Container,
+  List,
+  ListItem,
+} from "native-base";
 import { StyleSheet, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function StudentCard({ data }) {
   if (!data && Object.keys(data).length === 0) return null;
 
-  // console.log(data)
+  console.log(data);
 
   const {
     cellphone,
@@ -23,50 +32,84 @@ export default function StudentCard({ data }) {
   } = data;
 
   return (
-    <Content
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-      padder
-    >
-      <CardItem header style={styles.card}>
-        <Thumbnail
-          style={styles.image}
-          source={{
-            uri:
-              "https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png",
-          }}
-        />
-      </CardItem>
-      <Text style={styles.subtitles}>
-        {name} {lastName}
-      </Text>
-      <Text style={styles.titles}>{github}</Text>
-      <View style={{ flex: 1, flexDirection: "column" }}>
-        <Text style={styles.items}>Instructor</Text>
-        <Text style={styles.titles}>Leandro Alvarez</Text>
-      </View>
-      <View style={{ flex: 1, flexDirection: "column" }}>
-        <Text style={styles.items}>Modulo</Text>
-        <Text style={styles.titles}>Modulo 3</Text>
-      </View>
-      <View style={{ flex: 1, flexDirection: "column" }}>
-        <Text style={styles.items}>Grupo</Text>
-        <Text style={styles.titles}>FT-04</Text>
-      </View>
-      <View style={{ flex: 1, flexDirection: "column" }}>
-        <Text style={styles.items}>Fecha de Inicio</Text>
-        <Text style={styles.titles}>25/11/2020</Text>
-      </View>
-      <View style={{ flex: 1, flexDirection: "column" }}>
-        <Text style={styles.items}>Numero de Telefono</Text>
-        <Text style={styles.titles}>+541173693527</Text>
-      </View>
-      <Container style={styles.card1}>
-        <Text></Text>
-      </Container>
-    </Content>
+    <ScrollView>
+      <Content
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+        padder
+      >
+        <CardItem header style={styles.card}>
+          <Thumbnail
+            style={styles.image}
+            source={{
+              uri:
+                "https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png",
+            }}
+          />
+        </CardItem>
+        <Text style={styles.subtitles}>
+          {name} {lastName}
+        </Text>
+        <Text style={styles.titles}>{github}</Text>
+        <View style={{ flex: 1, flexDirection: "column" }}>
+          <Text style={styles.items}>Instructor</Text>
+          <Text style={styles.titles}>
+            {instructor && Object.keys(instructor).length > 0
+              ? `${instructor.firstName} ${instructor.lastName}`
+              : "ninguno"}
+          </Text>
+        </View>
+        <View style={{ flex: 1, flexDirection: "column" }}>
+          <Text style={styles.items}>Modulo</Text>
+          <Text style={styles.titles}>
+            {module ? module : "no tiene asignado ningun modulo"}
+          </Text>
+        </View>
+        <View style={{ flex: 1, flexDirection: "column" }}>
+          <Text style={styles.items}>Grupo</Text>
+          <Text style={styles.titles}>
+            {group
+              ? group
+              : "no tiene asignado ningun grupo, hable con administracion para un reembolso"}
+          </Text>
+        </View>
+        <View style={{ flex: 1, flexDirection: "column" }}>
+          <Text style={styles.items}>Fecha de Inicio</Text>
+          <Text style={styles.titles}>
+            {startDay
+              ? startDay.slice(0, 10)
+              : "que hace aqui si no tiene dia de inicio"}
+          </Text>
+        </View>
+        <View style={{ flex: 1, flexDirection: "column" }}>
+          <Text style={styles.items}>Numero de Telefono</Text>
+          <Text style={styles.titles}>
+            {cellphone ? cellphone : "coloque su numero de telefono"}
+          </Text>
+          {projectManagers && projectManagers.length > 0 ? (
+            <>
+              <Text style={styles.items}>PM's</Text>
+              <List>
+                {projectManagers.map((e) => (
+                  <ListItem>
+                    <Text style={styles.titles}>
+                      {e.firstName} {e.lastName}
+                    </Text>
+                  </ListItem>
+                ))}
+              </List>
+            </>
+          ) : (
+            <Text>"no tiene pm's asignados"</Text>
+          )}
+        </View>
+        <Container style={styles.card1}>
+          <Text></Text>
+        </Container>
+      </Content>
+    </ScrollView>
   );
 }
 
