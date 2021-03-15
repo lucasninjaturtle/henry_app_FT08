d45176c2d9b25cedc14bdc2a238898740b652442
Lucas <lucasmercado101@gmail.com>
2021-03-15 18:11:23 -0300

    fixed styles

--- web-app-admin/src/pages/EditEvent/EditEvent.jsx
+++ web-app-admin/src/pages/EditEvent/EditEvent.jsx
@@ -9,9 +9,13 @@ function CreateEvent() {
 
   return (
     <div className="h-full w-full flex">
-      <div className="m-auto w-full max-w-lg shadow-md flex">
-        <EditEventForm />
-        <Calendar />
+      <div className="m-auto px-5 w-full max-w-xl lg:max-w-7xl flex lg:flex-row flex-col">
+        <div class="lg:w-1/2 w-full">
+          <EditEventForm />
+        </div>
+        <div class="lg:w-1/2 w-full">
+          <Calendar />
+        </div>
       </div>
     </div>
   );
