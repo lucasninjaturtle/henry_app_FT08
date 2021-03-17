c07e773fe9e80655c659d3a22acc4e3f36f3ceee
lucasmercado101 <lucasmercado101@gmail.com>
2021-03-16 23:45:16 -0300

    populates for demo

--- api/src/routes/projectManager.ts
+++ api/src/routes/projectManager.ts
@@ -4,6 +4,7 @@ const router = Router();
 // Importo los controllers de cada ruta
 import { projectManagerController } from "../controller/projectManager";
 
+router.get("/", projectManagerController.getAllPms);
 router.get("/search", projectManagerController.searchPmByName);
 router.post("/bulk", projectManagerController.bulkCreate);
 
